6851fc26a088dcac2607f0cdf24b0315b7b87ace
Asylgareev Arthur <son.of.gallifrei@gmail.com>
2017-10-14 17:17:53 +0500

    Example of story config was added

---
+++ TextGameFramework.IO.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TextGameFramework.IO.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
