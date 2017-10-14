2454e0c905a90fc561c78291e7b3fd2d1ec84ecb
Asylgareev Arthur <son.of.gallifrei@gmail.com>
2017-10-14 20:58:47 +0500

    Simple unit test for the parsing of the correct yaml config was added

--- TextGameFramework.IO.Tests/YamlParserTests.cs
+++ TextGameFramework.IO.Tests/YamlParserTests.cs
@@ -7,15 +7,19 @@ namespace TextGameFramework.IO.Tests
 {
     public class YamlParserTests
     {
-        private void SetUpTest()
-        {
-            Directory.SetCurrentDirectory("../../../Parser/v0.0.1");
-        }
 
+        private const string _testDataFolder = "../../../Parser/v0.0.1/";
         [Fact]
         public void ShouldWork()
         {
-            SetUpTest();
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void ShouldWorkWhenCorrectFileIsGeiven()
+        {
+            var conf = YamlParser.Parse(Path.Combine(_testDataFolder, "simple-correct-example.yaml"));
+            //sanity check
             Assert.True(true);
         }
     }
