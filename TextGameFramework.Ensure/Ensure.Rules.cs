dc2682b3ee60304cc376a55507f402a5a87d0239
Asylgareev Arthur <son.of.gallifrei@gmail.com>
2017-10-15 19:19:54 +0500

    Utilitary functions from Ensure were extracted to its own file.  More checks were added.
    Utilitary functions from Ensure were extracted to its own file. 
     Ensure class was made partial
     Checks IsNonPositive IsNotNullOrEmpty FileExists IsNotNull were added
     All functions were populated with xml comments

---
+++ TextGameFramework.Ensure/Ensure.Rules.cs
@@ -0,0 +1,96 @@
+using System;
+using System.IO;
+
+
+namespace TextGameFramework.Ensure
+{
+    /// <summary>
+    /// This class Ensures that requested conditions are satisfied
+    /// </summary>
+    public static partial class Ensure
+    {       
+        /// <summary>
+        /// This functions ensures that value is positive
+        /// </summary>
+        /// <param name="valueName">Name of the variable being checked.</param>
+        /// <param name="value">Actual value of the variable being checked.</param>
+        /// <param name="parentType">Type of the class which contains the variable as property.</param>
+        /// <exception cref="TextGameFramework.Ensure.EnsureException">Thrown when value is nonpositive</exception> 
+        public static void IsPositive(string valueName, double value, Type parentType = null)
+        {
+            PerformEnsureCheck(valueName, value, (v) => v > 0, "Must be positive", parentType);
+        }
+
+        /// <summary>
+        /// This functions ensures that value is negative
+        /// </summary>
+        /// <param name="valueName">Name of the variable being checked.</param>
+        /// <param name="value">Actual value of the variable being checked.</param>
+        /// <param name="parentType">Type of the class which contains the variable as property.</param>
+        /// <exception cref="TextGameFramework.Ensure.EnsureException">Thrown when value is nonnegative</exception> 
+        public static void IsNegative(string valueName, double value, Type parentType = null)
+        {
+            PerformEnsureCheck(valueName, value, (v) => v < 0, "Must be negative", parentType);
+        }
+
+        /// <summary>
+        /// This functions ensures that value is nonnegative
+        /// </summary>
+        /// <param name="valueName">Name of the variable being checked.</param>
+        /// <param name="value">Actual value of the variable being checked.</param>
+        /// <param name="parentType">Type of the class which contains the variable as property.</param>
+        /// <exception cref="TextGameFramework.Ensure.EnsureException">Thrown when value is negative</exception> 
+        public static void IsNonNegative(string valueName, double value, Type parentType = null)
+        {
+            PerformEnsureCheck(valueName, value, (v) => v >= 0, "Must be nonnegative", parentType);
+        }
+
+        /// <summary>
+        /// This functions ensures that value is nonpositive
+        /// </summary>
+        /// <param name="valueName">Name of the variable being checked.</param>
+        /// <param name="value">Actual value of the variable being checked.</param>
+        /// <param name="parentType">Type of the class which contains the variable as property.</param>
+        /// <exception cref="TextGameFramework.Ensure.EnsureException">Thrown when value is positive</exception> 
+        public static void IsNonPositive(string valueName, double value, Type parentType = null)
+        {
+            PerformEnsureCheck(valueName, value, (v) => v < 0, "Must be nonpositive", parentType);
+        }
+
+        /// <summary>
+        /// This functions ensures that input string is not null or empty
+        /// </summary>
+        /// <param name="valueName">Name of the variable being checked.</param>
+        /// <param name="value">Actual value of the variable being checked.</param>
+        /// <param name="parentType">Type of the class which contains the variable as property.</param>
+        /// <exception cref="TextGameFramework.Ensure.EnsureException">Thrown when input string is null or empty</exception> 
+        public static void IsNotNullOrEmpty(string valueName, string value, Type parentType = null)
+        {
+            PerformEnsureCheck(valueName, value, (v) => !string.IsNullOrEmpty(value), "String must not be null or empty", parentType);
+        }
+
+        /// <summary>
+        /// This functions ensures that the input file exists
+        /// </summary>
+        /// <param name="valueName">Name of the variable being checked.</param>
+        /// <param name="value">Actual value of the variable being checked.</param>
+        /// <param name="parentType">Type of the class which contains the variable as property.</param>
+        /// <exception cref="TextGameFramework.Ensure.EnsureException">Thrown when input file does not exists</exception> 
+        public static void FileExists(string valueName, string value, Type parentType = null)
+        {
+            PerformEnsureCheck(valueName, value, (v) => File.Exists(value), "File must exist", parentType);
+        }
+
+        /// <summary>
+        /// This functions ensures that the input variable is not null
+        /// </summary>
+        /// <param name="valueName">Name of the variable being checked.</param>
+        /// <param name="value">Actual value of the variable being checked.</param>
+        /// <param name="parentType">Type of the class which contains the variable as property.</param>
+        /// <exception cref="TextGameFramework.Ensure.EnsureException">Thrown when input variable is null</exception> 
+        public static void IsNotNull<T>(string valueName, T value, Type parentType = null) where T : class
+        {
+            PerformEnsureCheck(valueName, value, (v) => v != null, "Variable must not be null", parentType);
+        }
+    }
+}
--- TextGameFramework.Ensure/Ensure.Utils.cs
+++ TextGameFramework.Ensure/Ensure.Utils.cs
@@ -4,18 +4,15 @@ using System.Linq;
 
 namespace TextGameFramework.Ensure
 {
-    public static class Ensure
+    public static partial class Ensure
     {
         private static string GetDescriptionFromType(string valueName, Type parentType)
         {
             var property = parentType.GetProperty(valueName);
             if (property == null)
-            {
                 return null;
-            }
 
             var descriptions = property.GetCustomAttributes(typeof(DescriptionAttribute), true);
-
             return !descriptions.Any() ? null : (descriptions[0] as DescriptionAttribute)?.Description;
         }
 
@@ -25,26 +22,14 @@ namespace TextGameFramework.Ensure
                 throw new EnsureException($"There is a problem with variable {valueName}! {validationProblem}! Current value {value}!");
             
             var description = GetDescriptionFromType(valueName, parentType);
-
             throw new EnsureException($"There is a problem with variable {valueName}({description})! {validationProblem}! Current value {value}!");
         }
 
         private static void PerformEnsureCheck<T>(string valueName, T value, Func<T,bool> condition, string valueProblem, Type parentType = null)
         {
             if(condition(value))
-                return;
-            
+                return;    
             ThrowEnsureException(valueName, value, valueProblem, parentType);
-        }
-
-        public static void IsPositive(string valueName, double value, Type parentType = null)
-        {
-            PerformEnsureCheck(valueName, value, (v) => v > 0, "Must be positive", parentType);
-        }
-
-        public static void IsNegative(string valueName, double value, Type parentType = null)
-        {
-            PerformEnsureCheck(valueName, value, (v) => v < 0, "Must be negative", parentType);
         }
     }
 }
